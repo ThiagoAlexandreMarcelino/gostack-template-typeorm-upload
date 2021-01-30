877844b0ce9ee23735ae82c8957b9296a83e5de6
Thiago A. Marcelino <thiagodistend@hotmail.com>
2021-01-29 21:52:14 -0300

    Desafio primeiro projeto note finalizado

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // Existe?
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+    // se sim
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
